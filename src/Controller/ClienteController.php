d51ac21040d6b2a5146677be6d47df9d13bb0e37
Iván Yamil Romero <31389316+knewlife@users.noreply.github.com>
2022-08-14 09:43:18 -0300

    desde vscode

---
+++ src/Controller/ClienteController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Cliente;
+use App\Repository\ClienteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClienteController extends AbstractController
+{
+    private EntityManagerInterface $em;
+    private ClienteRepository $cr;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $cr = $this->em->getRepository(Cliente::class);
+    }
+
+    #[Route('/cliente', name: 'app_cliente')]
+    public function index(): Response
+    {
+        $clientes = $this->cr->findAll();
+
+        return $this->render('cliente/index.html.twig', [
+            'controller_name' => 'ClienteController',
+            'clientes' => $clientes
+        ]);
+    }
+
+    #[Route('/cliente/{id}', name: 'app_cliente_view')]
+    public function view($id): Response
+    {
+        $cliente = $this->cr->find($id);
+
+        return $this->render('cliente/view.html.twig', [
+            'controller_name' => 'ClienteController',
+            'cliente' => $cliente
+        ]);
+    }
+}
